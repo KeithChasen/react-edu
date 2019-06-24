39109bf7931f7dda1dde49e02a2df9e8b4dcc64b
keithchasen <keithchasen89@gmail.com>
2019-06-24 23:16:28 +0300

    Handling squares state inside board

--- src/components/Game/Square.js
+++ src/components/Game/Square.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
 class Square extends Component {
-
     render() {
-        const { value } = this.props
         return (
-            <button className="square" onClick={() => {alert('click number: ' + value)}}>
-                {value}
+            <button
+                className="square"
+                onClick={() => this.props.onClick()}
+            >
+                {this.props.value}
             </button>
         );
     }
