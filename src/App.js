31a7730e94bc25dbc199cacbd8261ed59182d1a8
keithchasen <keithchasen89@gmail.com>
2019-06-24 21:53:52 +0300

    basic game markup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import './game.css'
 import TodoApp from "./components/TodoApp";
+import Game from "./components/Game/Game";
 
 function App() {
   return (
     <div className="App">
       <TodoApp/>
+      <Game/>
     </div>
   );
 }
