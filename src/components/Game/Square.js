aaecbc17c3fb8d2138f5b42d4ce01b31951838e0
keithchasen <keithchasen89@gmail.com>
2019-06-25 22:31:57 +0300

    make Square component functional

--- src/components/Game/Square.js
+++ src/components/Game/Square.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Square extends Component {
-    render() {
+function Square(props) {
         return (
             <button
                 className="square"
-                onClick={() => this.props.onClick()}
+                onClick={() => props.onClick()}
             >
-                {this.props.value}
+                {props.value}
             </button>
         );
-    }
 }
 
 export default Square
